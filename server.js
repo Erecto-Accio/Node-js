67112296b4a1c142b53a00ab213fffae4856c9f6
Erecto-Accio <toisiftasrik@gmail.com>
2024-06-28 15:45:39 +0600

    Simple form created after giving input on text field it redirects into new page

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.write(
-    "<html><head><title>Hiii</title></head><body><h1>Hello World</h1></body></html>"
-  );
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/message" && method === "POST") {
+    res.write(
+      `<html>
+    <head>
+    <title>Hiii</title>
+    </head>
+    <body>
+    <h1>Redirect Page</h1>
+    <h2> Welcome to the redirected page</h2>
+    </body>
+    </html > `
+    );
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    
+    <body>
+      <h1>Welcome page</h1>
+      <h2>Tousif Tasrik </h2>
+      <form action="/message" method="POST">
+        <input type="text" name="message" />
+        <button type="submit">Submit</button>
+      </form>
+    </body>
+    </html>
+      `);
   res.end();
 });
 
