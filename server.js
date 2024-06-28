ae69aab2b199d8b046e6fb3a00508bb61505fc1f
Erecto-Accio <toisiftasrik@gmail.com>
2024-06-28 16:48:21 +0600

    There was a mistake on the code fixed it and made two condition url || Also now i will parse the body and store the input field text into message.text

--- server.js
+++ server.js
@@ -4,39 +4,36 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
 
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy!!!");
-    res.write(
-      `<html>
-    <head>
-    <title>Hiii</title>
-    </head>
-    <body>
-    <h1>Redirect Page</h1>
-    <h2> Welcome to the redirected page</h2>
-    <h1 style="color:red">Check Message.txt</h1>
-    </body>
-    </html > `
-    );
+  if (url === "/") {
+    res.write(`<html>
+      <head>
+      <title>Welcome Page</title>
+      </head>
+      <body>
+      <form action="/message" method="POST">
+      <input type="text" name="message" />
+      <button type="submit">Submit</button>
+      </form>
+      </body>
+      </html>
+      `);
     return res.end();
   }
 
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy!!!");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
-  res.write(`<html>
-    <head>
-        <title>Welcome Page</title>
-    </head>
-    
+  res.write(`<html> 
+    <head><title>Intro page</title></head>
     <body>
-      <h1>Welcome page</h1>
+     <h1>Welcome page</h1>
       <h2>Tousif Tasrik </h2>
-      <form action="/message" method="POST">
-        <input type="text" name="message" />
-        <button type="submit">Submit</button>
-      </form>
-    </body>
-    </html>
-      `);
+      </body>
+      </html>`);
   res.end();
 });
 
