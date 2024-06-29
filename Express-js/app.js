4cb81bb160efccdf428260151596ebf1451e6bd8
Erecto-Accio <toisiftasrik@gmail.com>
2024-06-30 00:11:20 +0600

    Body parser used refactored the code

--- Express-js/app.js
+++ Express-js/app.js
@@ -1,19 +1,24 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
-// middleware
-app.use((req, res, next) => {
-  console.log("In the first middleware");
-  next(); // Allows the request to continue the next middleware
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /> <button type='submit'>Submit</button></form>"
+  );
 });
-app.use((req, res, next) => {
-  console.log("In the second middleware");
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res) => {
   res.send("<h1>Hello from the response</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Listening");
 });
