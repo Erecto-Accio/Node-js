a21793b6b37e587ba8ad2ebf1d2cb362ee83e7e3
Erecto-Accio <toisiftasrik@gmail.com>
2024-06-29 13:00:28 +0600

    next() function is pretty useful

--- Express-js/app.js
+++ Express-js/app.js
@@ -2,6 +2,15 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+// middleware
+app.use((req, res, next) => {
+  console.log("In the first middleware");
+  next(); // Allows the request to continue the next middleware
+});
+app.use((req, res, next) => {
+  console.log("In the second middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000, () => {
