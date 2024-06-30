ba5e19f79aa15cc7facafa6da7bf6686e397d2c3
Erecto-Accio <toisiftasrik@gmail.com>
2024-06-30 18:28:53 +0600

    Express router use and code organized

--- Express-js/app.js
+++ Express-js/app.js
@@ -2,23 +2,15 @@ const express = require("express");
 
 const app = express();
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+// adminRoutes
+const adminRouter = require("./routes/admin");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// shop routes
+const shopRouter = require("./routes/shop");
 
-app.use("/add-product", (req, res) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /> <button type='submit'>Submit</button></form>"
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res) => {
-  res.send("<h1>Hello from the response</h1>");
-});
-
+app.use(adminRouter);
+app.use(shopRouter);
 app.listen(3000, () => {
   console.log("Listening");
 });
