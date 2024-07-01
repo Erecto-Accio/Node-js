f96b45f9da7cfde77325452f9cf066a1224994a3
Erecto-Accio <toisiftasrik@gmail.com>
2024-07-02 01:53:47 +0600

    Redirect url changed

--- Express-js/routes/admin.js
+++ Express-js/routes/admin.js
@@ -7,7 +7,7 @@ router.get("/add-product", (req, res) => {
 
 router.post("/add-product", (req, res) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/shop");
 });
 
 module.exports = router;
